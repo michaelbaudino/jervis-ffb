d5a2429d429cd9fe67a711de1407042c82e8d83f
Christian Melchior <christian@ilios.dk>
2025-04-17 08:05:28 +0200

    Fix crash because Windows do not support app About dialogs

--- modules/jervis-ui/src/jvmMain/kotlin/com/jervisffb/main.kt
+++ modules/jervis-ui/src/jvmMain/kotlin/com/jervisffb/main.kt
@@ -2,8 +2,6 @@ package com.jervisffb
 
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
@@ -20,52 +18,21 @@ import com.jervisffb.ui.initApplication
 import com.jervisffb.ui.menu.BackNavigationHandler
 import com.jervisffb.ui.pixelsToDp
 import java.awt.Desktop
-import java.awt.desktop.AboutEvent
-import java.awt.desktop.AboutHandler
 
 
 fun main() {
-
-// TODO Create a better About page
-//    java.awt.Desktop.getDesktop().setAboutHandler {
-//
-//        val versionLabel = JLabel("<html><b>Version: 0.1.0.dev.local (dev build)<b></html>").apply {
-//            horizontalAlignment = SwingConstants.CENTER
-//            alignmentX = Component.CENTER_ALIGNMENT
-//            font = Font("SansSerif", Font.PLAIN, 12)
-//        }
-//
-//        val contentPanel = JPanel().apply {
-//            border = EmptyBorder(12, 12, 12, 12)
-//            layout = BoxLayout(this, BoxLayout.Y_AXIS)
-//            alignmentX = JPanel.CENTER_ALIGNMENT
-//
-////            add(iconLabel)
-//            add(Box.createVerticalStrut(10)) // Adds spacing
-//            add(versionLabel)
-//            add(Box.createVerticalStrut(5))
-////            add(descriptionLabel)
-//        }
-//
-//        val frame = JFrame("About Jervis Fantasy Football")
-//        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-//        frame.contentPane = contentPanel
-//        frame.pack()
-//        frame.isVisible = true
-//        frame.isResizable = false
-//    }
-    initApplication()
     try {
+        initApplication()
         application {
             val menuViewModel = MenuViewModel()
-            var showAbout by remember { mutableStateOf(false) }
             LaunchedEffect(Unit) {
                 if (Desktop.isDesktopSupported()) {
-                    Desktop.getDesktop().setAboutHandler(object : AboutHandler {
-                        override fun handleAbout(e: AboutEvent?) {
+                    val desktop = Desktop.getDesktop()
+                    if (desktop.isSupported(Desktop.Action.APP_ABOUT)) {
+                        desktop.setAboutHandler {
                             menuViewModel.showAboutDialog(true)
                         }
-                    })
+                    }
                 }
             }
             val scale = 1.22f
@@ -94,6 +61,7 @@ fun main() {
         }
     } catch (ex: Throwable) {
         // TODO Show crash dialog
+        throw ex
     }
 }
 
