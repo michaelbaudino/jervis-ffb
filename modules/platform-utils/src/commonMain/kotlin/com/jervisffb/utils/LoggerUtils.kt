877294e1c71641471e0d19eb8e690b81f38459b0
Christian Melchior <christianmelchior@gmail.com>
2025-06-14 20:04:04 +0200

    Make it more visible where log level is coming from

--- modules/platform-utils/src/commonMain/kotlin/com/jervisffb/utils/LoggerUtils.kt
+++ modules/platform-utils/src/commonMain/kotlin/com/jervisffb/utils/LoggerUtils.kt
@@ -3,11 +3,13 @@ package com.jervisffb.utils
 import co.touchlab.kermit.Logger
 import co.touchlab.kermit.Severity
 
+val DEFAULT_LOG_LEVEL = Severity.Debug
+
 // This needs to be expanded so we create a logger instance for each type
 // since it should also affect the output.
 val loggerInstance by lazy {
     Logger.apply {
-        setMinSeverity(Severity.Debug)
+        setMinSeverity(DEFAULT_LOG_LEVEL)
         getPlatformLogWriter()?.let { logger ->
             addLogWriter(logger)
         }
