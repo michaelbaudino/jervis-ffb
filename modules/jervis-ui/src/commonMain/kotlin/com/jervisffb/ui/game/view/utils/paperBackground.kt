8858204c1194392bcabbc4bbe8321f09e14a7280
Christian Melchior <christianmelchior@gmail.com>
2025-07-27 09:32:12 +0200

    Add clip support for the paper background

--- modules/jervis-ui/src/commonMain/kotlin/com/jervisffb/ui/game/view/utils/paperBackground.kt
+++ modules/jervis-ui/src/commonMain/kotlin/com/jervisffb/ui/game/view/utils/paperBackground.kt
@@ -1,10 +1,13 @@
 package com.jervisffb.ui.game.view.utils
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.ShaderBrush
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 import com.jervisffb.ui.game.view.JervisTheme
 import com.jervisffb.ui.menu.intro.createGrayscaleNoiseShader
@@ -12,20 +15,22 @@ import com.jervisffb.ui.menu.intro.createGrayscaleNoiseShader
 /**
  * Add noise to a background color so it mimics a paper-like texture.
  */
-fun Modifier.paperBackground(color: Color = JervisTheme.rulebookPaper): Modifier {
+fun Modifier.paperBackground(color: Color = JervisTheme.rulebookPaper, shape: Shape = RectangleShape): Modifier {
     val paperShader = createGrayscaleNoiseShader()
-    return this.drawBehind {
-        // Add desired background color
-        drawRect(color = color, size = size)
-        // Add semi-transparent noise on top
-        drawRect(
-            size = size,
-            brush = ShaderBrush(paperShader),
-            alpha = 0.3f,
-        )
-        // Re-add background color to make the noise blend more into the background
-        drawRect(color = color.copy(alpha = 0.5f), size = size)
-    }
+    return this
+        .clip(shape)
+        .drawBehind {
+            // Add desired background color
+            drawRect(color = color, size = size)
+            // Add semi-transparent noise on top
+            drawRect(
+                size = size,
+                brush = ShaderBrush(paperShader),
+                alpha = 0.3f,
+            )
+            // Re-add background color to make the noise blend more into the background
+            drawRect(color = color.copy(alpha = 0.5f), size = size)
+        }
 }
 
 fun Modifier.stoneBackground(): Modifier {
