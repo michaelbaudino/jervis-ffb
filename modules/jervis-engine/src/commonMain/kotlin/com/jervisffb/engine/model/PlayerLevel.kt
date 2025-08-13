4532b718f120c49342106e4b21f125d0ea2492ea
Christian Melchior <christianmelchior@gmail.com>
2025-08-13 12:01:51 +0200

    Add user visible description of Player levels

--- modules/jervis-engine/src/commonMain/kotlin/com/jervisffb/engine/model/PlayerLevel.kt
+++ modules/jervis-engine/src/commonMain/kotlin/com/jervisffb/engine/model/PlayerLevel.kt
@@ -1,11 +1,11 @@
 package com.jervisffb.engine.model
 
-enum class PlayerLevel {
-    ROOKIE,
-    EXPERIENCED,
-    VETERAN,
-    EMERGING_STAR,
-    STAR,
-    SUPER_STAR,
-    LEGEND,
+enum class PlayerLevel(val description: String) {
+    ROOKIE("Rookie"),
+    EXPERIENCED("Experienced"),
+    VETERAN("Veteran"),
+    EMERGING_STAR("Emerging Star"),
+    STAR("Star"),
+    SUPER_STAR("Super Star"),
+    LEGEND("Legend"),
 }
