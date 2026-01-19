00719de6000edeeb85f1b09b8e286bfcc860619e
Christian Melchior <christianmelchior@gmail.com>
2026-01-19 21:46:35 +0100

    Do now show field on-hover effect when action wheel is visible

--- modules/jervis-ui/src/commonMain/kotlin/com/jervisffb/ui/game/view/field/FieldHoverUnderlayLayer.kt
+++ modules/jervis-ui/src/commonMain/kotlin/com/jervisffb/ui/game/view/field/FieldHoverUnderlayLayer.kt
@@ -3,14 +3,22 @@ package com.jervisffb.ui.game.view.field
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.jervis.generated.SettingsKeys
 import com.jervisffb.engine.model.locations.FieldCoordinate
 import com.jervisffb.ui.SETTINGS_MANAGER
+import com.jervisffb.ui.game.view.ActionWheelUiStateData
+import com.jervisffb.ui.game.view.HideActionWheel
 import com.jervisffb.ui.game.view.JervisTheme
+import com.jervisffb.ui.game.view.ShowActionWheel
 import com.jervisffb.ui.game.viewmodel.FieldViewModel
+import kotlinx.coroutines.launch
 
 /**
  * Layer 5: Hover Underlay:
@@ -32,7 +40,21 @@ fun FieldHoverUnderlayLayer(
     // When the Action Wheel is showing, we do not want to show the hover effect (regardless of it being enabled or not)
     // as it looks confusing with too many things happening at once. The onHover on players will still trigger to show
     // player cards.
-    val isShowingActionWheel by vm.sharedFieldData.isContentMenuVisible
+    var showingPrimaryWheel by remember { mutableStateOf(false) }
+    val showingSecondaryWheel by remember { vm.sharedFieldData.isContentMenuVisible }
+    LaunchedEffect(vm) {
+        launch {
+            vm.actionWheelViewModel.observe().collect {
+                showingPrimaryWheel = when (it) {
+                    is ActionWheelUiStateData,
+                    ShowActionWheel -> true
+                    else -> false
+                }
+            }
+        }
+    }
+
+    val isShowingActionWheel = showingPrimaryWheel || showingSecondaryWheel
     if (!isShowingActionWheel && highlightedSquare != null && showHover && highlightedSquare!!.isOnField(vm.game.rules)) {
         Box(
             modifier = Modifier
