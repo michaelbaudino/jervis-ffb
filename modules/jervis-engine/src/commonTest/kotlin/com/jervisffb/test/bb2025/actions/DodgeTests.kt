88b47d1c93a459e9efd9efffbef5ca946953bad3
Christian Melchior <christianmelchior@gmail.com>
2026-05-21 20:45:57 +0200

    Cannot use Dodge on Stumble if Distracted (FAQ May 2026)
    This was already working, so this commit just adds a test capturing the behavior.

--- modules/jervis-engine/src/commonTest/kotlin/com/jervisffb/test/bb2025/actions/DodgeTests.kt
+++ modules/jervis-engine/src/commonTest/kotlin/com/jervisffb/test/bb2025/actions/DodgeTests.kt
@@ -1,20 +1,30 @@
 package com.jervisffb.test.bb2025.actions
 
-import com.jervisffb.engine.actions.NoRerollSelected
+import com.jervisffb.engine.actions.DirectionSelected
 import com.jervisffb.engine.actions.PlayerActionSelected
 import com.jervisffb.engine.actions.PlayerSelected
 import com.jervisffb.engine.ext.d6
+import com.jervisffb.engine.ext.dblock
 import com.jervisffb.engine.ext.playerId
+import com.jervisffb.engine.model.Direction
 import com.jervisffb.engine.model.locations.PitchCoordinate
 import com.jervisffb.engine.rules.common.actions.PlayerStandardActionType
 import com.jervisffb.engine.rules.common.rerolls.RegularTeamReroll
+import com.jervisffb.engine.rules.common.skills.SkillType
 import com.jervisffb.test.JervisGameBB2025Test
 import com.jervisffb.test.SmartMoveTo
+import com.jervisffb.test.activatePlayer
+import com.jervisffb.test.dodge
 import com.jervisffb.test.ext.rollForward
+import com.jervisffb.test.followUp
 import com.jervisffb.test.moveTo
+import com.jervisffb.test.standardBlock
 import com.jervisffb.test.utils.TeamRerollSelected
+import com.jervisffb.test.utils.assertCoordinates
 import com.jervisffb.test.utils.assertFallenOver
+import com.jervisffb.test.utils.assertKnockedDown
 import com.jervisffb.test.utils.assertStanding
+import com.jervisffb.test.utils.makeDistracted
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -45,8 +55,7 @@ class DodgeTests: JervisGameBB2025Test() {
         assertTrue(rules.isMarked(player))
         controller.rollForward(
             *moveTo(14, 5),
-            6.d6,
-            NoRerollSelected()
+            *dodge(6.d6),
         )
         assertEquals(PitchCoordinate(14, 5), player.coordinates)
         player.assertStanding()
@@ -78,8 +87,7 @@ class DodgeTests: JervisGameBB2025Test() {
         assertEquals(2, rules.calculateMarks(state, awayTeam, PitchCoordinate(11, 5)))
         controller.rollForward(
             *moveTo(11, 5),
-            4.d6, // Need 5+ to dodge
-            NoRerollSelected()
+            *dodge(4.d6), // Need 5+ to dodge
         )
         player.assertFallenOver()
     }
@@ -95,8 +103,7 @@ class DodgeTests: JervisGameBB2025Test() {
         assertEquals(PitchCoordinate(13, 4), player.coordinates)
         // Then roll for dodge
         controller.rollForward(
-            6.d6,
-            NoRerollSelected()
+            *dodge(6.d6),
         )
         assertEquals(PitchCoordinate(13, 4), player.coordinates)
         player.assertStanding()
@@ -108,8 +115,7 @@ class DodgeTests: JervisGameBB2025Test() {
             PlayerSelected("A1".playerId),
             PlayerActionSelected(PlayerStandardActionType.MOVE),
             *moveTo(12, 4),
-            1.d6, // Fail dodge
-            NoRerollSelected()
+            *dodge(1.d6), // Fail dodge
         )
         val player = awayTeam["A1".playerId]
         player.assertFallenOver()
@@ -129,5 +135,25 @@ class DodgeTests: JervisGameBB2025Test() {
         val player = awayTeam["A1".playerId]
         player.assertStanding()
         assertEquals(PitchCoordinate(12, 4), player.coordinates)
+    }
+
+    @Test
+    fun doesNotWorkAgainstStumbleIfDistracted() {
+        val attacker = state.getPlayerById("A1".playerId)
+        val defender = state.getPlayerById("H1".playerId)
+        defender.apply {
+            addSkill(SkillType.DODGE)
+            makeDistracted()
+        }
+        controller.rollForward(
+            *activatePlayer(attacker, PlayerStandardActionType.BLOCK),
+            *standardBlock(defender, 5.dblock),
+            DirectionSelected(Direction.UP_LEFT),
+            followUp(false),
+        )
+        attacker.assertCoordinates(13, 5)
+        attacker.assertStanding()
+        defender.assertCoordinates(11, 4)
+        defender.assertKnockedDown()
     }
 }
